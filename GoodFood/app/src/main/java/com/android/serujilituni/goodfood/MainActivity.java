3e4c582bb9d0b530c7b3fc4ec0b31099dda1384c
Sergio Muñoz <59663486+selcox@users.noreply.github.com>
2021-01-14 14:03:37 +0100

    Created android project

---
+++ GoodFood/app/src/main/java/com/android/serujilituni/goodfood/MainActivity.java
@@ -0,0 +1,14 @@
+package com.android.serujilituni.goodfood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
