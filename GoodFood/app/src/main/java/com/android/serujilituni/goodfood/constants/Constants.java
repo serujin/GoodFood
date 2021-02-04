9988680738030c8a5c515dbfbc6cdc7a9751e01d
Sergio Muñoz <59663486+selcox@users.noreply.github.com>
2021-02-04 12:45:37 +0100

    Added user and order constants

---
+++ GoodFood/app/src/main/java/com/android/serujilituni/goodfood/constants/Constants.java
@@ -0,0 +1,14 @@
+package com.android.serujilituni.goodfood.constants;
+
+public class Constants {
+    //USER INFO
+    public static final String USER_ID = "user_id";
+    public static final String USER_NAME = "user_name";
+    public static final String USER_EMAIL = "user_email";
+    public static final String USER_ADDRESS = "user_address";
+    public static final String USER_PASSWORD = "user_password";
+    //ORDER INFO
+    public static final String ORDER_ID = "order_id";
+    public static final String ORDER_PRICE = "order_price";
+    public static final String ORDER_DATETIME = "order_datetime";
+}
