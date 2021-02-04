f6b7abf1f70677cafbcbe10ed4d05ba34a2c85d5
Manuel Martín Benito <manuelmarb@gmail.com>
2021-02-04 12:45:23 +0100

    adding Restaurants and Plates constants

---
+++ GoodFood/app/src/main/java/com/android/serujilituni/goodfood/constants/Constants.java
@@ -0,0 +1,14 @@
+package com.android.serujilituni.goodfood.constants;
+
+public class Constants {
+
+    private static final String RESTAURANT_ID = "restaurant_id";
+    private static final String RESTAURANT_NAME = "restaurant_name";
+    private static final String RESTAURANT_ADDRESS = "restaurant_address";
+
+    private static final String PLATE_ID = "plate_id";
+    private static final String PLATE_NAME = "plate_name";
+    private static final String PLATE_PRICE = "plate_price";
+    private static final String PLACE_DESC = "plate_desc";
+
+}
