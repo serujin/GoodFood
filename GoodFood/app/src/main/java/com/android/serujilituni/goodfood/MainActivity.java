95b4b8c0aa2fb556e33465c35972be3e8fb4ea1f
Sergio Muñoz <59663486+selcox@users.noreply.github.com>
2021-02-06 16:33:13 +0100

    Project correctly setup

--- GoodFood/app/src/main/java/com/android/serujilituni/goodfood/MainActivity.java
+++ GoodFood/app/src/main/java/com/android/serujilituni/goodfood/MainActivity.java
@@ -3,12 +3,18 @@ package com.android.serujilituni.goodfood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.serujilituni.goodfood.store.DBManager;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DBManager.getInstance();
     }
 }
--- GoodFood/app/src/main/java/com/android/serujilituni/goodfood/store/DBManager.java
+++ GoodFood/app/src/main/java/com/android/serujilituni/goodfood/store/DBManager.java
@@ -1,17 +1,35 @@
 package com.android.serujilituni.goodfood.store;
 
+import android.util.Log;
+
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Random;
 
 public class DBManager {
 
     private static DBManager instance;
-    private DatabaseReference mDatabase;
+    private DatabaseReference db;
 
     public static DBManager getInstance(){
         if(DBManager.instance==null) {
             DBManager.instance = new DBManager();
         }
         return DBManager.instance;
+    }
+
+    private DBManager() {
+        initDatabase();
+        Log.i("ADAd", "dasdawd");
+    }
+
+    private void initDatabase() {
+        this.db = FirebaseDatabase.getInstance().getReference();
+        if(this.db == null) {
+            System.out.println("DB CREATED");
+        }
+        this.db.
     }
 
     public void storeOrder(Object[] data) {
