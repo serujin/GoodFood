e603bd569d242959ffda6d1fc23744908e0e25ca
Sergio Muñoz <59663486+selcox@users.noreply.github.com>
2021-02-09 17:56:57 +0100

    asda

--- GoodFood/app/src/main/java/com/android/serujilituni/goodfood/activities/restaurant/RestaurantsActivity.java
+++ GoodFood/app/src/main/java/com/android/serujilituni/goodfood/activities/restaurant/RestaurantsActivity.java
@@ -1,19 +1,21 @@
 package com.android.serujilituni.goodfood.activities.restaurant;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.android.serujilituni.goodfood.R;
 import com.android.serujilituni.goodfood.adapters.RestaurantAdapter;
 import com.android.serujilituni.goodfood.items.RestaurantItem;
+import com.android.serujilituni.goodfood.utils.Utils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,10 +27,7 @@ public class RestaurantsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
-        DrawerLayout drawer = findViewById(R.id)
-    }
-}
-        /**RecyclerView rv = findViewById(R.id.restaurants_layout);
+        RecyclerView rv = findViewById(R.id.restaurants_layout);
         rv.setAdapter(new RestaurantAdapter(generateRestaurants(10))); //TODO: CHANGE GENERATE RESTAURANTS FOR APP CACHE RESTAURANTS
         rv.setLayoutManager(new LinearLayoutManager(this));
         rv.setHasFixedSize(true);
