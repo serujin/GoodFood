0a8626c0d8e72d8a59967cc8b44b16e58c711c69
Manuel Martín Benito <manuelmarb@gmail.com>
2021-02-11 13:48:34 +0100

    Added styles

--- GoodFood/app/src/main/java/com/android/serujilituni/goodfood/activities/ordersummary/OrderSummaryActivity.java
+++ GoodFood/app/src/main/java/com/android/serujilituni/goodfood/activities/ordersummary/OrderSummaryActivity.java
@@ -16,23 +16,26 @@ public class OrderSummaryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_summary);
         AppCache.getInstance().setContext(this);
+        /**
+
         new OrderSummaryController(
                 getButtons(),
                 findViewById(R.id.address_info_et),
-                findViewById(R.id.plates_rv_layout), 
+                findViewById(R.id.plates_rv_layout),
                 getTextviews()
         );
+         */
     }
 
     private Button[] getButtons() {
-        return new Button[] {
+        return new Button[]{
                 findViewById(R.id.pay_btn),
                 findViewById(R.id.cancel_btn)
         };
     }
 
     private TextView[] getTextviews() {
-        return new TextView[] {
+        return new TextView[]{
                 findViewById(R.id.distance_info_tv),
                 findViewById(R.id.price_info_tv),
                 findViewById(R.id.price_deliver_info_tv),
